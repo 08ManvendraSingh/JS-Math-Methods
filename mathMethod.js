4d867a7b66b9be98b5a7f8eef5f52d6ecf45e915
Manvendra Singh <08singhmanvendra@gmail.com>
2024-02-13 12:01:32 +0530

    initial commit

---
+++ mathMethod.js
@@ -0,0 +1,69 @@
+// Math Methods
+// Math properties
+// 1.Math.PI
+document.write(Math.PI +'<br>');
+console.log(Math.PI);
+
+// 2.Math.SQRT2
+document.write(Math.SQRT2 +'<br>');
+console.log(Math.SQRT2);
+
+// 3.Math.SQRT1_2
+document.write(Math.SQRT1_2 +'<br>');
+console.log(Math.SQRT1_2);
+
+// 4.Math.LN2
+document.write(Math.LN2 +'<br>');
+console.log(Math.LN2);
+
+// 5.Math.LN10
+document.write(Math.LN10 +'<br>');
+console.log(Math.LN10);
+
+// Math Methods
+// 1.Math.round()
+var a=23.46;
+document.write(Math.round(a)+'<br>');
+console.log(Math.round(a));
+
+// 2.Math.ceil()
+var a=23.16;
+document.write(Math.ceil(a)+'<br>');
+console.log(Math.ceil(a));
+
+// 3.Math.floor()
+var a=23.76;
+document.write(Math.floor(a)+'<br>');
+console.log(Math.floor(a));
+
+// 4.Math.trunc()
+var a=3.53;
+document.write(Math.trunc(a)+'<br>');
+console.log(Math.trunc(a));
+
+// 5.Math.pow()
+var a=3;
+document.write(Math.pow(a,2) +'<br>');
+console.log(Math.pow(a,2));
+
+// 6.Math.sqrt()
+var a=64;
+document.write(Math.sqrt(a)+'<br>');
+console.log(Math.sqrt(a));
+
+// 7.Math.abs()
+var a=-22;
+document.write(Math.abs(a)+'<br>');
+console.log(Math.abs(a));
+
+// 8.Math.max()
+document.write(Math.max(2,32,34,45,3)+'<br>');
+console.log(Math.max(2,32,34,45,3));
+
+// 9.Math.min()
+document.write(Math.min(12,2,34,45,3)+'<br>');
+console.log(Math.min(12,2,34,45,3))
+
+// 10.Math.random()
+document.write(Math.random()+'<br>');
+console.log(Math.random());
